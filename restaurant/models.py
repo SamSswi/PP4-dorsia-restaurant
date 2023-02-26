8d7209a696ce3e200c0e1dc7f179b75c3d1cfb75
SamSswi <sadovoisamuel7@gmail.com>
2023-02-26 01:40:14 +0000

    Add Drink model

--- restaurant/models.py
+++ restaurant/models.py
@@ -10,6 +10,15 @@ MEAL_TYPE = (
     (3, "Side Dish"),
     (4, "Dessert")
     )
+DRINK_TYPE = (
+    (0, "Wine"),
+    (1, "Classic Cocktail"),
+    (2, "Special Cocktail"),
+    (3, "Aperitif"),
+    (4, "Digestif"),
+    (5, "Sparkling Wine"),
+    (6, "Beer")
+    )
 
 
 class Booking(models.Model):
@@ -19,6 +28,8 @@ class Booking(models.Model):
     date = models.DateField()
     time = models.TimeField()
     status = models.IntegerField(choices=STATUS, default=0)
+
+# add __str__ to bookings
 
 
 class Meal(models.Model):
@@ -32,3 +43,16 @@ class Meal(models.Model):
 
     def __str__(self):
         return self.meal_name
+
+
+class Drink(model.Models):
+    drink_name = models.CharField(max_length=80)
+    drink_description = models.CharField()
+    drink_type = models.IntegerField(choices=DRINK_TYPE)
+    allergens = model.CharField()
+
+    class Meta:
+        ordering = ['drink_type', 'drink_name']
+
+    def __str__(self):
+        return self.drink_name
