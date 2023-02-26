7cd95314de3dde39d42ceac4b45eb66727653097
SamSswi <sadovoisamuel7@gmail.com>
2023-02-26 01:31:39 +0000

    Add Meal model

--- restaurant/models.py
+++ restaurant/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Pending"), (1, "Confirmed"))
+MEAL_TYPE = (
+    (0, "Appetizer"),
+    (1, "Salad"),
+    (2, "Main Course"),
+    (3, "Side Dish"),
+    (4, "Dessert")
+    )
 
 
 class Booking(models.Model):
@@ -12,3 +19,16 @@ class Booking(models.Model):
     date = models.DateField()
     time = models.TimeField()
     status = models.IntegerField(choices=STATUS, default=0)
+
+
+class Meal(models.Model):
+    meal_name = models.CharField(max_length=100)
+    meal_description = models.CharField()
+    meal_type = models.IntegerField(choices=MEAL_TYPE)
+    allergens = models.CharField()
+
+    class Meta:
+        ordering = ['meal_type', 'meal_name']
+
+    def __str__(self):
+        return self.meal_name
