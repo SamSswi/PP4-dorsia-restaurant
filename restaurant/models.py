86022f8c1dcc859ca3ffd029a571048c5c7830dc
SamSswi <sadovoisamuel7@gmail.com>
2023-02-26 01:04:18 +0000

    Add Booking model

--- restaurant/models.py
+++ restaurant/models.py
@@ -3,3 +3,12 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Pending"), (1, "Confirmed"))
+
+
+class Booking(models.Model):
+    customer_id = models.ForeignKey(
+        User, on_delete=models.Cascade, related_name='restaurant_booking')
+    number_of_guests = models.IntegerField(default=1)
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.IntegerField(choices=STATUS, default=0)
