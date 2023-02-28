15b5d0b653ddd981327a772bd0d0fb6ab31de4bd
SamSswi <sadovoisamuel7@gmail.com>
2023-02-28 23:38:43 +0000

    Register the models in admin.py

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Booking, Meal, Drink
 
-# Register your models here.
+admin.site.register(Booking)
+admin.site.register(Meal)
+admin.site.register(Drink)
