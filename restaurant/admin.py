95986def18f4ef18e613590d977c6edc4e85042e
SamSswi <sadovoisamuel7@gmail.com>
2023-03-05 21:32:39 +0000

    Add list_display property to meal and drink

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -7,13 +7,17 @@ from django_summernote.admin import SummernoteModelAdmin
 class BookingAdmin(SummernoteModelAdmin):
     # https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+FST101+2021_T1/courseware/b31493372e764469823578613d11036b/09e0a94c7dbd4b969b8358a0cf5660b2/?child=first
     list_filter = ('status', 'date', 'time')
+    # add list_display
 
 
 @admin.register(Meal)
 class MealAdmin(SummernoteModelAdmin):
     list_filter = ['meal_type']
+    list_display = ('meal_name', 'meal_type')
 
 
 @admin.register(Drink)
 class MealAdmin(SummernoteModelAdmin):
     list_filter = ['drink_type']
+    list_display = ('drink_name', 'drink_type')
+
