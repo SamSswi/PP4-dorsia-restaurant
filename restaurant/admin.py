63152fb4ad867bb13359ed72eb90165f9d6e4eac
SamSswi <sadovoisamuel7@gmail.com>
2023-03-05 17:03:10 +0000

    Install Summernote and add filters for bookings

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Booking, Meal, Drink
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Booking)
+
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+    # https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+FST101+2021_T1/courseware/b31493372e764469823578613d11036b/09e0a94c7dbd4b969b8358a0cf5660b2/?child=first
+    list_filter = ('status', 'date', 'time')
+
+
 admin.site.register(Meal)
 admin.site.register(Drink)
