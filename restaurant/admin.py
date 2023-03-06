039031a22bdbe872b9e81357f095ae771b94db11
SamSswi <sadovoisamuel7@gmail.com>
2023-03-06 08:39:19 +0000

    Add actions functionality to the Booking model

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -8,6 +8,11 @@ class BookingAdmin(SummernoteModelAdmin):
     # https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+FST101+2021_T1/courseware/b31493372e764469823578613d11036b/09e0a94c7dbd4b969b8358a0cf5660b2/?child=first
     list_filter = ('status', 'date', 'time')
     # add list_display
+    actions = ['confirm_booking']
+
+# https://github.com/Code-Institute-Solutions/Django3blog/tree/master/05_building_the_admin_site
+    def confirm_booking(self, request, querryset):
+        querryset.update(confirmed=True)
 
 
 @admin.register(Meal)
