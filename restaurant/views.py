20e6a8e0e8619198f1b60785cae0cee21c81458b
SamSswi <sadovoisamuel7@gmail.com>
2023-03-07 16:17:51 +0000

    Add view and template for index.hmtl

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+# https://github.com/Code-Institute-Solutions/Django3blog/tree/master/05_building_the_admin_site
+# from django.views import generic
+# from .models import Booking
 
-# Create your views here.
+
+def home_page(request):
+    return render(request, 'index.html')
