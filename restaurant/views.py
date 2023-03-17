b90211b84e9864dab23542f10a059516f3126726
SamSswi <sadovoisamuel7@gmail.com>
2023-03-17 14:35:05 +0000

    Update the reservation view and template so the user can make a booking

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,10 +1,10 @@
 # https://docs.djangoproject.com/en/4.1/topics/auth/default/#the-login-required-decorator/
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 # https://github.com/Code-Institute-Solutions/Django3blog/tree/master/05_building_the_admin_site
 from django.views import generic, View
 from .models import Meal, Drink, Booking
-from datetime import date, timedelta
+from datetime import date, timedelta, time, datetime
 
 
 def home_page(request):
@@ -28,9 +28,27 @@ def reservation(request):
         booking_email = request.POST.get('email')
         booking_phone_number = request.POST.get('phone_number')
         number_of_guests = request.POST.get('number_of_guests')
-        booking_date = request.POST.get('date')
-        time = request.POST.get('time')
+        booking_date_string = request.POST.get('booking-date')
+        time_string = request.POST.get('booking-time')
         comment = request.POST.get('comment')
+        # https://www.programiz.com/python-programming/datetime/strptime
+        # https://www.geeksforgeeks.org/how-to-convert-datetime-to-date-
+        # in-python/ .date() method
+        booking_date = datetime.strptime(
+            booking_date_string, '%Y-%m-%d').date()
+        time = datetime.strptime(time_string, '%I:%M %p').time()
+        Booking.objects.create(
+            # https://stackoverflow.com/questions/12615154/how-to-get-the-currently-logged-in-users-user-id-in-django
+            customer_id=request.user,
+            booking_full_name=booking_full_name,
+            booking_email=booking_email,
+            booking_phone_number=booking_phone_number,
+            number_of_guests=number_of_guests,
+            booking_date=booking_date,
+            time=time,
+            comment=comment
+        )
+        return redirect('home_page')
     # variables
     max_guest_num = tuple(range(1, 21))
     hour_tuple = (
