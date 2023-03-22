c6627262432a016e89f42172391360a54299678f
SamSswi <sadovoisamuel7@gmail.com>
2023-03-22 00:45:24 +0000

    Add functionality to delete_reservations.js to show and hide the modal

---
+++ static/js/delete_reservation.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const myModal = new bootstrap.Modal(document.getElementById('exampleModalCenter'))
+    const closeModalButtons = document.querySelectorAll('[data-dismiss="modal"]')
+    for (let i = 0; i < closeModalButtons.length; i++) {
+        closeModalButtons[i].addEventListener('click', function () {
+            myModal.hide()
+        })
+    }
+    document.getElementById("delete-btn").addEventListener("click", myModal.show())
+})
